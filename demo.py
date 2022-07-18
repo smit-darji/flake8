ad42a056b34f0abf397766f75b3d0d8b1bab9cdc
smit-darji <98390067+smit-darji@users.noreply.github.com>
2022-07-18 17:28:11 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,8 @@
-A = 1.5
-B = 6.2
-print(A)
-print(B)
+import boto3
+s3 = boto3.client('s3')
+response = s3.list_buckets()
+
+# Output the bucket names
+print('Existing buckets:')
+for bucket in response['Buckets']:
+    print(f'  {bucket["Name"]}')
