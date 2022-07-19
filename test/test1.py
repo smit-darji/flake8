d93a824098f0f1e4fff083296f69795de8190289
smit-darji <98390067+smit-darji@users.noreply.github.com>
2022-07-19 12:54:33 +0530

    Create test1.py

---
+++ test/test1.py
@@ -0,0 +1,10 @@
+import pytest
+def test_file1_method1():
+    x=3
+    y=6
+    assert x+3 == y,"test success"
+    assert x == y,"test failed"
+def test_file1_method2():
+    x=3
+    y=6
+    assert x+3 == y,"test success"
